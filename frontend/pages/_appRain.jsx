2ad16a0a3e125d4fe06333ca577c1b40a7ef786f
Sai Krishna <saikrishnas2508@gmail.com>
2023-03-18 16:41:01 +0530

    Final Commit

--- frontend/pages/_appRain.jsx
+++ frontend/pages/_appRain.jsx
@@ -18,9 +18,9 @@ const { chains, provider } = configureChains(
 */
 
 // Imports for Rainbowkit and Wagmi
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
+// import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+// import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+// import { publicProvider } from "wagmi/providers/public";
 
 // Setting Chains
 const { chains, provider } = configureChains(
