b493c00e6ba45ab1fdbf64107a0d2878e9dcfa37
Sai Krishna <saikrishnas2508@gmail.com>
2023-03-18 16:25:45 +0530

    First commit

---
+++ backend/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "goerli",
+  networks: {
+    goerli: {
+      url: process.env.QUICKNODE_HTTP_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
